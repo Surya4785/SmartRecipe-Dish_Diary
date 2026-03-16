cafb287f6faef4d2636d76cf21600df8ce7bfeec
Nawab Surya <suryaprakash8707yadav@gmail.com>
2026-03-17 02:15:05 +0530

    Added Error Handling

--- frontend/SmartRecipe-Dish_Diary/src/App.jsx
+++ frontend/SmartRecipe-Dish_Diary/src/App.jsx
@@ -1,61 +1,87 @@
-import React from 'react'
-import './App.css'
-import {createBrowserRouter,RouterProvider} from "react-router-dom"
-import Home from './pages/Home'
-import MainNavigation from './components/MainNavigation'
-import axios from 'axios'
-import  AddFoodRecipe  from './pages/AddFoodRecipe'
-import EditRecipe from './pages/EditRecipe'
-import RecipeDetails from './pages/RecipeDetails'
+import React from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import MainNavigation from "./components/MainNavigation";
+import axios from "axios";
+import AddFoodRecipe from "./pages/AddFoodRecipe";
+import EditRecipe from "./pages/EditRecipe";
+import RecipeDetails from "./pages/RecipeDetails";
 
+const API = "http://localhost:5000";
 
-const getAllRecipes=async()=>{
-  let allRecipes=[]
-  await axios.get('http://localhost:5000/recipe').then(res=>{
-    allRecipes=res.data
-  })
-  return allRecipes
-}
+// GET ALL RECIPES
+const getAllRecipes = async () => {
+  try {
+    const res = await axios.get(`${API}/recipe`);
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+    return [];
+  }
+};
 
-const getMyRecipes=async()=>{
-  let user=JSON.parse(localStorage.getItem("user"))
-  let allRecipes=await getAllRecipes()
-  return allRecipes.filter(item=>item.createdBy===user._id)
-}
+// GET MY RECIPES
+const getMyRecipes = async () => {
+  try {
+    let user = JSON.parse(localStorage.getItem("user"));
+    let allRecipes = await getAllRecipes();
 
-const getFavRecipes=()=>{
-  return JSON.parse(localStorage.getItem("fav"))
-}
+    if (!user) return [];
 
-const getRecipe=async({params})=>{
-  let recipe;
-  await axios.get(`http://localhost:5000/recipe/${params.id}`)
-  .then(res=>recipe=res.data)
+    return allRecipes.filter((item) => item.createdBy === user._id);
+  } catch (error) {
+    console.error("Error loading my recipes:", error);
+    return [];
+  }
+};
 
-  await axios.get(`http://localhost:5000/user/${recipe.createdBy}`)
-  .then(res=>{
-    recipe={...recipe,email:res.data.email}
-  })
+// GET FAVORITE RECIPES
+const getFavRecipes = () => {
+  try {
+    const fav = JSON.parse(localStorage.getItem("fav"));
+    return fav ? fav : [];
+  } catch (error) {
+    console.error("Error loading favorite recipes:", error);
+    return [];
+  }
+};
 
-  return recipe
-}
+// GET SINGLE RECIPE
+const getRecipe = async ({ params }) => {
+  try {
+    let recipe;
 
-const router=createBrowserRouter([
-  {path:"/",element:<MainNavigation/>,children:[
-    {path:"/",element:<Home/>,loader:getAllRecipes},
-    {path:"/myRecipe",element:<Home/>,loader:getMyRecipes},
-    {path:"/favRecipe",element:<Home/>,loader:getFavRecipes},
-    {path:"/addRecipe",element:<AddFoodRecipe/>},
-    {path:"/editRecipe/:id",element:<EditRecipe/>},
-    {path:"/recipe/:id",element:<RecipeDetails/>,loader:getRecipe}
-  ]}
- 
-])
+    const res = await axios.get(`${API}/recipe/${params.id}`);
+    recipe = res.data;
+
+    if (recipe.createdBy) {
+      const userRes = await axios.get(`${API}/user/${recipe.createdBy}`);
+      recipe = { ...recipe, email: userRes.data.email };
+    }
+
+    return recipe;
+  } catch (error) {
+    console.error("Error loading recipe:", error);
+    return null;
+  }
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainNavigation />,
+    children: [
+      { path: "/", element: <Home />, loader: getAllRecipes },
+      { path: "/myRecipe", element: <Home />, loader: getMyRecipes },
+      { path: "/favRecipe", element: <Home />, loader: getFavRecipes },
+      { path: "/addRecipe", element: <AddFoodRecipe /> },
+      { path: "/editRecipe/:id", element: <EditRecipe /> },
+      { path: "/recipe/:id", element: <RecipeDetails />, loader: getRecipe }
+    ]
+  }
+]);
 
 export default function App() {
-  return (
-   <>
-   <RouterProvider router={router}></RouterProvider>
-   </>
-  )
+  return <RouterProvider router={router} />;
 }
